51a2f9ff629176792acbd910b6eef232ebaf4105
adxxtya <andronaut9@gmail.com>
2024-02-19 17:12:01 +0530

    Commenting port in docker to work with AWS App Runner

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+const port = 8080;
 
 app.get('/', (req, res) => {
-  res.json({ 'hey': 'Hello World!' })
-})
+  res.json({ 'hey': 'Hello World!' });
+});
+
+app.get('/health-check', (req, res) => {
+  const currentDate = new Date().toISOString();
+  res.status(200).json({ 'status': 'ok', 'currentDate': currentDate });
+});
 
 app.listen(port, () => {
-  console.log(`Example listening on port ${port}`)
-})
+  console.log(`Example listening on port ${port}`);
+});
